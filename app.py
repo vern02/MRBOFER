bc43cb7c7144880d74c1dda32e16d025cdae2507
vernsl <107494341+vern02@users.noreply.github.com>
2024-12-02 01:46:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,10 +43,28 @@ df = pd.read_csv("Stress.csv")
 #     "Current CGPA", "Waiver/Scholarship", "Anxiety Label", "Stress Label", "Depression Label"
 # ])
 
-# Initialize Firebase
-cred = credentials.Certificate("mrbofer-65af7-4e552797743e.json")
+# Access Firebase credentials from the secrets
+cred = credentials.Certificate({
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+})
+
+# Initialize Firebase app
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
+    
+# # Initialize Firebase
+# cred = credentials.Certificate("mrbofer-65af7-4e552797743e.json")
+# if not firebase_admin._apps:
+#     firebase_admin.initialize_app(cred)
 
 # Function to validate email format using regular expressions
 def is_valid_email(email):
